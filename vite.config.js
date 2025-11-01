b52eef53bd7afaf7664d222464068fa15ab2be25
luckytsori8@gmail.com <luckytsori8@gmail.com>
2025-11-01 17:03:59 +0300

    Add ecommerce backend

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // change default port 5173 → 3000
+    port: 5000, // change default port 5173 → 3000
   },
 });
 
