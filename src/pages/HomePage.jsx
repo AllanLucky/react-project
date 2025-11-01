baf1613daf0bd017504f7912ffef67535fc61e93
luckytsori8@gmail.com <luckytsori8@gmail.com>
2025-11-01 17:13:18 +0300

    Fetch data from the backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,13 @@ import { products } from '../../starting-coder/data/products.js'
 import './HomePage.css';
 
 const HomePage = () => {
+
+  fetch('http://localhost:3000/api/products')
+    .then((response) => {
+      return response.json();
+    }).then((data) => {
+      console.log(data);
+    });
   return (
     <>
       <title>Home Page</title>
