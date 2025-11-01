abaec1c076fcd67ffce72fb83277e93928362d6a
luckytsori8@gmail.com <luckytsori8@gmail.com>
2025-11-01 13:52:56 +0300

    update TrackingPage component to use React Router Links and JSX className attributes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import CheckoutPage from './pages/CheckoutPage'
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import CheckoutPage from './pages/CheckoutPage';
+import OrdersPage from './pages/OrdersPage'; // ✅ Added this import
+import TrackingPage from './pages/TrackingPage'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/orders" element={<OrdersPage />} /> {/* ✅ Fixed space */}
+      <Route path="/tracking" element={<TrackingPage />} /> {/* ✅ Fixed space */}
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
